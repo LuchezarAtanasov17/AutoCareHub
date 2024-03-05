c29c4e9cc83db4106a99adb6746c1fff58baf123
Лъчезар Атанасов <lachezaratanasov05@pmgkk.com>
2024-03-05 21:03:54 +0200

    Add service functionality for listing, getting and deleting users.

---
+++ AutoCareHub.Services.Impl/Users/UserService.cs
@@ -0,0 +1,51 @@
+﻿using AutoCareHub.Data;
+using AutoCareHub.Data.Models;
+using AutoCareHub.Services.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoCareHub.Services.Impl.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly AutoCareHubDbContext _context;
+
+        public UserService(AutoCareHubDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<List<User>> ListUsersAsync()
+        {
+            var users = await _context.Users
+                .ToListAsync();
+
+            return users;
+        }
+
+        public async Task<User> GetUserAsync(Guid id)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (user == null)
+            {
+                throw new ObjectNotFoundException(nameof(user));
+            }
+
+            return user;
+        }
+
+        public async Task DeleteUserAsync(Guid id)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == id);
+
+            if (user == null) 
+            {
+                throw new ObjectNotFoundException(nameof(user));
+            }
+
+            _context.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ AutoCareHub.Services/Users/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using AutoCareHub.Data.Models;
+
+namespace AutoCareHub.Services.Users
+{
+    public interface IUserService
+    {
+        Task<List<User>> ListUsersAsync();
+
+        Task<User> GetUserAsync(Guid id);
+
+        Task DeleteUserAsync(Guid id);
+    }
+}
