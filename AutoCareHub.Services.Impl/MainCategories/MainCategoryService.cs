989f9629cc1defd40d7a81ac230e6d3a6716275c
Лъчезар Атанасов <lachezaratanasov05@pmgkk.com>
2024-03-05 09:40:21 +0200

    Add service functionality for main categories.

---
+++ AutoCareHub.Services.Impl/MainCategories/MainCategoryService.cs
@@ -0,0 +1,72 @@
+﻿using AutoCareHub.Data;
+using AutoCareHub.Data.Models;
+using AutoCareHub.Services.MainCategories;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoCareHub.Services.Impl.MainCategories
+{
+    public class MainCategoryService : IMainCategoryService
+    {
+        private readonly AutoCareHubDbContext _context;
+
+        public MainCategoryService(AutoCareHubDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<List<MainCategory>> ListMainCategoriesAsync(Guid? serviceId = null)
+        {
+            var mainCategories = await _context.MainCategories
+                .Include(x => x.CategoryServices)
+                .ToListAsync();
+
+            if (serviceId != null)
+            {
+                mainCategories = await _context.MainCategoryServices
+                    .Where(x => x.ServiceId == serviceId)
+                    .Select(x => x.MainCategory)
+                    .ToListAsync();
+            }
+
+            return mainCategories;
+        }
+
+        public async Task<MainCategory> GetMainCategoryAsync(Guid id)
+        {
+            var mainCategory = await _context.MainCategories
+                 .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (mainCategory == null)
+            {
+                throw new ObjectNotFoundException(nameof(mainCategory));
+            }
+
+            return mainCategory;
+        }
+
+        public async Task CreateMainCategoryAsync(CreateMainCategoryRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            await _context.AddAsync(request);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteMainCategoryAsync(Guid id)
+        {
+            var mainCategory = _context.MainCategories
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (mainCategory == null)
+            {
+                throw new ObjectNotFoundException(nameof(mainCategory));
+            }
+
+            _context.Remove(mainCategory);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ AutoCareHub.Services/MainCategories/IMainCategoryService.cs
@@ -0,0 +1,15 @@
+﻿using AutoCareHub.Data.Models;
+
+namespace AutoCareHub.Services.MainCategories
+{
+    public interface IMainCategoryService
+    {
+        Task<List<MainCategory>> ListMainCategoriesAsync(Guid? serviceId = null);
+
+        Task<MainCategory> GetMainCategoryAsync(Guid id);
+
+        Task CreateMainCategoryAsync(CreateMainCategoryRequest request);
+
+        Task DeleteMainCategoryAsync(Guid id);
+    }
+}
